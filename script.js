486c9f81643532b05227eb22903d359e53d36b35
Ermazza <81581059+Ermazza@users.noreply.github.com>
2026-08-18 21:36:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -71,3 +71,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   renderServices();
 });
+const bioText = document.getElementById("bioText");
+const bioToggle = document.getElementById("bioToggle");
+
+if (bioText && bioToggle) {
+  bioToggle.addEventListener("click", () => {
+    bioText.classList.toggle("expanded");
+
+    bioToggle.textContent =
+      bioText.classList.contains("expanded")
+        ? "Mostra meno"
+        : "Leggi di più";
+  });
+}
